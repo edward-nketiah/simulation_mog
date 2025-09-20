abe006ba2428bd2877236d0ebd1e410f74ff2f08
eddy22 <72153099+edward-nketiah@users.noreply.github.com>
2025-09-20 13:57:07 +0800

    Update mog.m

--- mog.m
+++ mog.m
@@ -1,9 +1,7 @@
 function [p, mu_mat, var_mat,temp, totprob] = mog(data, P1, n,num_comp)
 
 d = size(data, 2);       
-%num_comp = 100;            % define number of components if not passed
-% Initialize mixing proportions
-%num_comp = k1 + 1;  % or k2 + 1
+
 % The length of p here adjust based on the component of each model 
 if isempty(P1)
    p = ones(1, num_comp) / num_comp;
@@ -55,20 +53,7 @@ while num_iter <= max_it && deltol > tol
         mat = center_temp' * diag(gamma(:, i)) * center_temp;
         var(:, :, i) = mat ./ sum(gamma(:, i))+ epsilon * eye(d);           
     end
-    % disp(size(p))
-    % disp(size(prob_coef))
-    % p = p(:)';           % ensure 1×num_comp
-    % prob_coef = prob_coef(:)';   % ensure 1×num_comp
-    % 
-    % % Always reshape to be consistent row vector
-    % p = p(:)';
-    % prob_coef = prob_coef(:)';
-    % 
-    % % % If something collapsed:
-    % if length(prob_coef) == 1 && length(p) > 1
-    %     prob_coef = prob_coef * ones(1, numel(p));
-    % end
-    % get the tolerance
+    
     delvar = max(max(max(abs(var - var_mat))));
     delmu = max(max(abs(mu_old - mu_p)));
     delpi = max(abs(p - prob_coef));
@@ -80,11 +65,3 @@ while num_iter <= max_it && deltol > tol
     var_mat = var;
 end
 
-% compute the intensity function here 
-% Keep separate intensities:
-% lambda_b = n_b * sum(totprob, 2);   % N_data × 1
-% lambda_o = (n_o / n_data) * sum(totprob_o, 2);  % N_data × 1
-% 
-% lambda = [lambda_b; lambda_o];
-% %log_likelihood
-% log_likelihood = sum(log(lambda(lambda~= 0))) - (n_b + n_o/n_data);
